d2a3943b658b6296b4ebb215ed0875952cab725f
JumaPackla <dariobahramkatani@gmail.com>
2025-09-29 11:23:05 +0100

    changes

--- content_scripts/content.js
+++ content_scripts/content.js
@@ -3,18 +3,20 @@ chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
         const question_container = document.getElementsByClassName("question-content-container row");
         if (question_container.length > 0) {
             const contents = question_container[0].getElementsByClassName("content-value");
-            const contexts = []
+            let contexts = ""
             for (let i = 0; i < contents.length; i++) {
                 const context = contents[i].textContent
-                contexts.push(context)
+                contexts += context + "\n"
             }
-            // const answer_container = question_container[0].getElementsByClassName("no-print-row");
-            // print("hello", answer_container)
-            // for (let i = 0; i < answer_container.length; i++) {
-            //     const answer = answer_container[i].textContent
-            //     print(answer)
-            //     contexts.push(answer)
-            // }
+            
+            let answer_options = ""
+            const answer_container = question_container[0].getElementsByClassName("no-print-row");
+            console.log(answer_container)
+            for (let i = 0; i < answer_container.length; i++) {
+                const answer = answer_container[i].textContent
+                answer_options += answer
+            }
+            console.log(answer_options)
             sendResponse({ contexts });
         } else {
             sendResponse({ contexts: ["Question container not found"]});
